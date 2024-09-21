987f50af5874b6b73db97b8ba740aca6545505b5
Dartty123 <dariyatamanuk@icloud.com>
2024-09-21 21:48:12 +0300

    dd

--- main.py
+++ main.py
@@ -2,29 +2,28 @@ from flask import Flask,  render_template
 from app.data import db
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="app/static", template_folder="app/templates")
+
 
 @app.get("/")
 def index():
     return render_template("index.html", title = "Вас вітає піцерія")
 
-
 @app.get("/menu/")
 def menu():
-    pizzas = [
-        {"name": "Гавайська", "price": 35, "ingredients":"Курка, ананас, томат, сир"},
-        {"name": "Цезаро", "price": 40, "ingredients":"Соус'Цезар', листя салату, помідори, пармезан"},
-        {"name": "Маргарита", "price": 20, "ingredients":"Томат, помідори, сир"},
-        {"name": "Mілано", "price": 25, "ingredients":"Cоус, ковбаса 'Мілано', гриби, сир"},
-]
+    pizza_db = db.get_PIZZA()
+    pizzas = []
+    for pizza in pizza_db:
+        pizzas.append(
+            {"name": pizza[1], "ingredients": pizza[2], "price": pizza[3]}
+        )
+
     context = {
         "pizzas": pizzas,
         "title": "меню"
     }
     return render_template("menu.html", **context)
 
-db.create_table()
-db.insert_data (**{"name": "Гавайська", "price": 35, "ingredients":"Курка, ананас, томат, сир"})
-db.insert_data (**{"name": "Цезаро", "price": 40, "ingredients":"Соус'Цезар', листя салату, помідори, пармезан"})  
-db.insert_data (**{"name": "Маргарита", "price": 20, "ingredients":"Томат, помідори, сир"})
-db.insert_data (**{"name": "Mілано", "price": 25, "ingredients":"Cоус, ковбаса 'Мілано', гриби, сир"})  
+
+if __name__ == "__main__":
+    app.run(debug=True)
