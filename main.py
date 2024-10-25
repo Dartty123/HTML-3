63f2a45ca85d0fe71be8acca191d39df2ebd0523
Dartty123 <dariyatamanuk@icloud.com>
2024-10-25 15:26:11 +0300

    ..л

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask ,render_template ,request ,redirect ,url_for
 
 from app.routers.pizza import pizza_route 
 from app.models.pizza import Pizza
@@ -13,4 +13,30 @@ app.register_blueprint(pizza_route)
 if __name__ == "__main__":
     create_db()
     app.run(debug=True)
-    
+    @app.get("/")
+    def index():
+        context = {
+            "question": "Яка піца тобі найбільше подобаєтся",
+            "answers": ["Гавайська, Морепіца, Велика піца на компію"]
+    }
+        return render_template("index.html", **context)
+
+@app.get("/add_vote/")
+def add_vote():
+    vote = request.args.get("answer")
+    with open("data/answers.txt", "a", encoding="utf-8") as file:
+        file.write(vote + "\n")
+
+    return redirect(url_for("results"))
+
+
+@app.get("/results/")
+def results():
+    with open("data/answers.txt", "r", encoding="utf-8") as file:
+        answers = file.readlines()
+
+    return render_template("results.html", answers=answers)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
